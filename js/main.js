8d007ca0a5b29424ade628df64a20386ba4eacd5
Simone <simone.bervi@gmail.com>
2021-01-12 19:22:03 +0100

    Navbar finished

--- js/main.js
+++ js/main.js
@@ -2,7 +2,18 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        cryptoCurrency: [],
+        navbarData: [
+            {
+                home: 'Home',
+                about: 'About Me',
+                testmonials: 'Testimonials',
+                blog: 'My Blog',
+                meet: 'Meetups',
+                shop: 'Shop',
+                contact: 'Contact me',
+                logo: 'logo.png',
+            },
+        ],
     },
     methods: {
 
